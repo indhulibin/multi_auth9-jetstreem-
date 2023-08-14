37bec931565f1f74c0f2bc2eff2f960c2c197ff5
Indhu <indhumohan19@gmail.com>
2023-08-14 12:14:14 +0930

    all files

--- 2login/app/Providers/FortifyServiceProvider.php
+++ 2login/app/Providers/FortifyServiceProvider.php
@@ -13,6 +13,12 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
 
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Laravel\Fortify\Actions\AttemptToAuthenticate;
+use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
+use App\Http\Controllers\AdminController;
+use Auth;
+
 class FortifyServiceProvider extends ServiceProvider
 {
     /**
@@ -20,7 +26,10 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->when([AdminController::class,AttemptToAuthenticate::class,
+        RedirectIfTwoFactorAuthenticatable::class])->needs(StatefulGuard::class)->give(function(){
+            return Auth::guard('admin');
+        });
     }
 
     /**
